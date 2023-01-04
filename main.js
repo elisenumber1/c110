afbd66fa042cc87f6df3c756c2f6fe710401b8b1
ShenhElise <100390294+elisenumber1@users.noreply.github.com>
2023-01-03 16:59:48 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-function preload(){
+nose_x=0;
+nose_y=0;
 
+function preload(){
+	mustache=loadImage("https://i.postimg.cc/MTNX7kSF/Funny-retro-hair-mustache-on-transparent-background-PNG.png");
 }
 
 function draw(){
 	image(video,0,0,300,300);
+	image(mustache, nose_x-10, nose_y-5, 30, 20);
 }
 
 function setup(){
@@ -25,8 +29,10 @@ function modelloaded(){
 function gotposes(results){
 	if(results.length>0){
 		console.log(results);
+		nose_x=results[0].pose.nose.x;
+		nose_y=results[0].pose.nose.y;
 		console.log("nose x="+results[0].pose.nose.x);
-		console.log("nose y="+results[0].pose.nose.x);
+		console.log("nose y="+results[0].pose.nose.y);
 
 	}
 }
